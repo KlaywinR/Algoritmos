d06a6a9a0b4a73c2296012421ad67758d06d9909
Klaywin Ryan <klaywindias@gmail.com>
2025-09-22 19:34:43 -0300

    Create ex01.c

---
+++ ALG/ex01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+    int a = 5;  // inteiro 
+    int b = 12; // inteiro natural
+    int *p; // ponteiro de p
+    int *q;   // ponteiro de q 
+    p = &a; // nesta parte, p está apontando para o endereço de memória de A, e q e está apontando para o emndereço de memória de B
+    p = &b;
+
+    int c = *p + *q;
+      
+    printf("O valor de c eh: %.2f ", c);
+
+    return 0;
+}
