2a7ab70b3097bb9e3514968f01c54681914c967b
Klaywin Ryan <klaywindias@gmail.com>
2025-10-23 09:50:44 -0300

    Create q.04.c

---
+++ PROVA/q.04.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(){
+    int baseMaior, baseMenor,altura;
+    double area;
+  
+    printf("Digite a base maior: ");
+    scanf("%d", &baseMaior);
+
+    printf("Digite a base menor: ");
+    scanf("%d", &baseMenor);
+
+    printf("Digite a altura: ");
+    scanf("%d", &altura);
+
+ area = CalcularArea(&baseMaior, &baseMenor, &altura);
+
+    printf("A area do trapezio eh: %.2f\n", area);
+
+}
+
+float CalcularArea(int *BaseMaior, int *BaseMenor, int *Altura) {
+    float area;
+    area = ((*BaseMaior + *BaseMenor) * (*Altura)) / 2.0;
+    return area;
+}
