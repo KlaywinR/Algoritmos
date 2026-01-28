5fb5dc80bdf8ea4be4e3802e504691a963e4fde6
Klaywin Ryan <klaywindias@gmail.com>
2026-01-28 16:52:06 -0300

    add merge_sort.py

--- DESAFIOS/merge_sort.py
+++ DESAFIOS/merge_sort.py
@@ -13,15 +13,20 @@ class Solution(object):
         - next: Referência usada para o nó seguinte.
     """
     def merge_two_list(self, list1, list2):
+        """Recebe duas listas encadeadas"""
         assistant = ListNode(0)
-        actual= assistant
-        
+        """Nó aux - valor de sentinela"""
+        actual = assistant
+        """esse é o ponteiro que aponta para a lista que vai caminhando."""
         while list1 and list2:
+            """enquanto as listas tiverem elementos há comparação."""
             if list1.val <= list2.val:
+                """Compara os valores do nó atual."""
+                """Liga o próximo no ao menor valor e avança a list 1"""
                 actual.next = list1
                 list1 = list1.next
-                
             else:
+                """Liga o nó de list 2 e avança"""
                 actual.next = list2
                 list2 = list2.next
             actual = actual.next
@@ -29,6 +34,5 @@ class Solution(object):
         actual.next = list1 or list2
         
         return assistant.next
-        
-            
-            
+    
+    
